f0836dc44ce621c3b88b9ff88803c8053bbfe6bd
aaaa <aaaa>
2022-06-29 16:32:56 +0900

    Add "🧑‍💻AboutDeveloperPublisher"

--- TapTemperature/🛠Menu/📄Information/📄InformationMenu.swift
+++ TapTemperature/🛠Menu/📄Information/📄InformationMenu.swift
@@ -70,10 +70,97 @@ struct 📄InformationMenu: View {
                 } label: {
                     Label("Source code", systemImage: "doc.plaintext")
                 }
+                
+                
+                NavigationLink {
+                    🧑‍💻AboutDeveloperPublisher()
+                } label: {
+                    Label("Developer / Publisher", systemImage: "person")
+                }
             }
             .navigationTitle("Information")
         } label: {
             Label("Information", systemImage: "doc")
         }
+    }
+}
+
+
+struct 🧑‍💻AboutDeveloperPublisher: View {
+    var body: some View {
+        List {
+            Section {
+                Text("Individual")
+            } header: {
+                Text("The System")
+            }
+            
+            
+            Section {
+                Text("山下 亮")
+                
+                Text("やました りょう (ひらがな)")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+                
+                Text("Yamashita Ryo (alphabet)")
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+            } header: {
+                Text("Name")
+            } footer: {
+                Text("only one person")
+            }
+            
+            
+            Section {
+                HStack {
+                    Text("age")
+                    Spacer()
+                    Text("about 28")
+                        .foregroundStyle(.secondary)
+                }
+                
+                HStack {
+                    Text("country")
+                    Spacer()
+                    Text("Japan")
+                        .foregroundStyle(.secondary)
+                }
+                
+                HStack {
+                    Text("native language")
+                    Spacer()
+                    Text("Japanese")
+                        .foregroundStyle(.secondary)
+                }
+                
+                HStack {
+                    Text("income")
+                    Spacer()
+                    Text("almost nothing")
+                        .foregroundStyle(.secondary)
+                }
+            } header: {
+                Text("identity / circumstance / background")
+            } footer: {
+                Text("As of 2021")
+            }
+            
+            
+            Section {
+                Image("Developer_Publisher")
+                    .resizable()
+                    .frame(width: 90, height: 90)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
+                    .padding()
+                    .opacity(0.5)
+            } header: {
+                Text("Image")
+            } footer: {
+                Text("Taken on 2021-11")
+            }
+        }
+        .navigationTitle("Developer / Publisher")
     }
 }
